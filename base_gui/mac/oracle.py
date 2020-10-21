572e4fd1fd7eb3c1e60d850269dae0eb61f3d0a9
David Zwart <davidzwa@gmail.com>
2020-10-21 12:22:25 +0200

    Cleanup

--- base_gui/mac/oracle.py
+++ base_gui/mac/oracle.py
@@ -38,14 +38,13 @@ class Oracle(object):
         # - name & default state
         # - provide transmission rate 'lambda'
         # Init nodes
-        # - assign valid/separated 2D position
+        # - assign valid/separated random 2D position
         assert len(self.actors) != 0 or regenerate is True
         if regenerate is True:
             self.node_positions_np = self.__generate_positions(self.num_nodes, self.positional_spread)
             for i, position in enumerate(self.node_positions_np):
                 identifier = "N{}".format(i)
                 self.actors.append(ActorStateHistory(identifier, position))
-            # - random positions
 
         # - calculate neighbours to nodes based on transmission range cutoff
         # - set IDLE state to each node
@@ -53,7 +52,6 @@ class Oracle(object):
             copy_actors = [x for i2, x in enumerate(self.actors) if i != i2]
             for neighbour_actor in copy_actors:
                 dist = self.__calc_actor_distances(actor, neighbour_actor)
-                print(dist)
                 if dist < transmission_range:
                     actor.add_neighbour(neighbour_actor.state)
                     neighbour_actor.add_neighbour(actor.state)
@@ -90,14 +88,14 @@ class Oracle(object):
                     packet_length=packet_length
                 )
             # 3) Transform state, flatten list and store list of state of nodes in 2D time-node state matrix
+            # - Update any nodes with outstanding 'arrivals', MAC update and/or 'out-of-range messages'
             for actor in self.actors:
                 actor.progress_time(self.delta_time)
+            # 4) Save state
             for actor in self.actors:
                 actor.save_state_to_history()
-            # - Update any nodes with outstanding 'arrivals' or their own 'dead messages'
-            # for i, actor in enumerate(self.actors):
-            #     actor.
         pass
+
         # Store (optional)
         # - store as JSON with parameters
 
@@ -113,9 +111,6 @@ if __name__ == '__main__':
     oracle.preprocess(time_steps=500, delta_time=1,
                       packet_length=5, transmission_range=60,
                       transmission_chance=0.15)
-
     for actor in oracle.actors:
-        state: FrozenActorState
-        print(len(actor.history))
-        # for state in actor.history:
-        #     print(len(state.queued_messages))
+        for state in actor.history:
+            print(len(state.queued_messages))
