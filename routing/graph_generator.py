db9784be22fe70b61b8bb77c13562ace38aa33b5
Niek van der Plas <n.vanderplas@decos.com>
2020-11-03 15:11:10 +0100

    A bizzilion changes to dsdv

--- routing/graph_generator.py
+++ routing/graph_generator.py
@@ -1,7 +1,9 @@
+from typing import Sequence, Tuple
 import networkx as nx
 import random
 import math
 import time
+from copy import deepcopy
 
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
@@ -10,6 +12,7 @@ import matplotlib.pyplot as plt
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QSpacerItem, QSizePolicy, QPushButton
 from PyQt5.QtCore import QSize, QTimer
 
+kcount = 0
 
 class Node:
     def __init__(self, key, x, y):
@@ -20,47 +23,194 @@ class Node:
 
 
 class RoutingEntry:
-    def __init__(self, destination, next_hop, metric):
+    def __init__(self, destination, next_hop, metric, sequence_number=None):
         self.destination = destination
         self.next_hop = next_hop
         self.metric = metric
+        self.sequence_number = sequence_number
 
-
-class GraphGenerator(FigureCanvas):
-    def __init__(self, parent=None, width=10, height=8, dpi=100):
-        self.graph = nx.Graph()
-        self.window_width = 300
-        self.window_height = 300
-        self.node_count = 30
-        self.max_edge_distance = 50
-        self.max_x_displacement = 25
-        self.max_y_displacement = 25
-
-        self.nodes = {}
+class Graph:
+    def __init__(self, node_count, window_height, window_width, edge_distance):
+        self.nodes = []
         self.edges = []
-        self.edge_distances = []
+        self.node_count = node_count
+        self.window_height = window_height
+        self.window_width = window_width
+        self.max_edge_distance = edge_distance
 
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
+    def remove_random_node(self):
+        if len(self.nodes) > 0:
+            node_index = random.randint(0, len(self.nodes) - 1)
+            node_key = self.nodes[node_index].key
+            del self.nodes[node_index]
 
-        FigureCanvas.setSizePolicy(
-            self, QSizePolicy.Expanding, QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
+            for i, edge in enumerate(self.edges):
+                if node_key in edge:
+                    del self.edges[i]
+
+            for node in self.nodes:
+                for i, entry in enumerate(node.table):
+                    if entry.destination == node_key:
+                        del node.table[i]
+        return self.nodes
+
+    def set_node_count(self, count):
+        self.node_count = count
 
     def calculate_distance(self, x1, y1, x2, y2):
         distance = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
         return distance
 
-    def get_nodes(self, node_count, min_x, max_x, min_y, max_y):
-        nodes = []
-        for i in range(node_count):
-            x = random.randint(min_x, max_x)
-            y = random.randint(min_y, max_y)
-            new_node = Node(i, x, y)
-            nodes.append(new_node)
-        return nodes
+    def update_sequence(self, sequence, fail=False):
+        sequence_nr = int(sequence.split('-')[1])
+        if sequence_nr % 2 == 1 and fail:
+            sequence_nr += 2
+        elif sequence_nr % 2 == 0 and fail:
+            sequence_nr += 1
+        elif sequence_nr % 2 == 1 and not fail:
+            sequence_nr += 1
+        else:
+            sequence_nr += 2
+        return sequence.split('-')[0] + '-' + str(sequence_nr).zfill(3)
+
+    def get_sequence_number(self, sequence):
+        return int(sequence.split('-')[1])
+
+    def broadcast_table(self):
+        for i in range(self.node_count - 1): #|V| - 1 is enough
+            for node in self.nodes:
+                for entry in node.table:
+                    if entry.metric == 1:
+                        index = [i for i,x in enumerate(self.nodes) if self.nodes[i].key == entry.destination][0]
+                        self.share_table(node, self.nodes[index])
+
+    def get_node_by_key(self, key):
+        result = None
+        for node in self.nodes:
+            if node.key == key:
+                result = node
+        return result
+
+    def broadcast_to_neighbours(self, node):
+        for entry in node.table:
+            if entry.metric == 1:
+                self.share_table(node, self.get_node_by_key(entry.destination))
+
+    def broadcast_broken_link(self, node1, node2):
+        print("Broken: ", node1.key, node2.key)
+        for node1_entry in node1.table:
+            if node1_entry.destination == node2.key or node1_entry.next_hop == node2.key:
+                node1_entry.metric = float("inf")
+                node1_entry.sequence_number = self.update_sequence(node1_entry.sequence_number, True)
+                node1_entry.next_hop = None
+
+        for node2_entry in node2.table:
+            if node2_entry.destination == node1.key or node2_entry.next_hop == node1.key:
+                node2_entry.metric = float("inf")
+                node2_entry.sequence_number = self.update_sequence(node2_entry.sequence_number, True)
+                node2_entry.next_hop = None
+    
+        self.broadcast_to_neighbours(node1)
+        self.broadcast_to_neighbours(node2)
+
+    def broadcast_new_link(self, node1, node2):
+        for node1_entry in node1.table:
+            if node1_entry.destination == node2.key:
+                node1_entry.metric = 1
+                node1_entry.sequence_number = self.update_sequence(node1_entry.sequence_number, False)
+                node1_entry.next_hop = node2.key
+
+        for node2_entry in node2.table:
+            if node2_entry.destination == node1.key:
+                node2_entry.metric = 1
+                node2_entry.sequence_number = self.update_sequence(node2_entry.sequence_number, False)
+                node2_entry.next_hop = node1.key
+    
+        self.broadcast_to_neighbours(node1)
+        self.broadcast_to_neighbours(node2)
+
+
+    def share_table(self, node1, node2):
+        broadcast = False
+        for i, node2_entry in enumerate(node2.table):
+            node1_entry = node1.table[i]
+
+            node1_number = self.get_sequence_number(node1_entry.sequence_number)
+            node2_number = self.get_sequence_number(node2_entry.sequence_number)
+            if node1_number > node2_number:
+                # New sequence, so update table in node 2
+                node2_entry.metric = node1_entry.metric + 1
+                if node1_entry.metric != float("inf"):
+                    node2_entry.next_hop = node1.key
+                else:
+                    node2_entry.next_hop = None
+                node2_entry.sequence_number = node1_entry.sequence_number
+                broadcast = True
+
+            if node2_entry.next_hop == node1.key:
+                if node1.table[i].metric == float("inf"):
+                    node2_entry.metric = float("inf")
+                    node2_entry.next_hop = None
+                    node2_entry.sequence_number = self.update_sequence(node2_entry.sequence_number, True)
+                    broadcast = True
+        
+        if broadcast:
+            self.broadcast_to_neighbours(node2)
+
+
+    def periodic_update(self):
+        for node in self.nodes:
+            for entry in node.table:
+                if int(entry.destination) == int(node.key):
+                    entry.sequence_number = self.update_sequence(entry.sequence_number, False)
+                
+            self.broadcast_to_neighbours(node)
+
+    def add_to_table(self, new_node):
+        for node in self.nodes:
+            if node.key != new_node.key:
+                new_entry = RoutingEntry(new_node.key, None, float('inf'), str(new_node.key) + '-000')
+                node.table.append(new_entry)
+        for node in self.nodes:
+            if node.key == new_node.key:
+                new_entry = RoutingEntry(node.key, node.key, 0, str(new_node.key) + '-000')
+                new_node.table.append(new_entry)                        
+            else:
+                new_entry = RoutingEntry(node.key, None, float('inf'), str(node.key) + '-000')
+                new_node.table.append(new_entry)
+        self.broadcast_to_neighbours(new_node)
+
+
+    def fill_table(self):
+        for node in self.nodes:
+            for i in range(len(self.nodes)):
+                if self.nodes[i].key != node.key:
+                    node.table.append(RoutingEntry(self.nodes[i].key, None, float('inf'), str(self.nodes[i].key) + '-000'))
+                else:
+                    node.table.append(RoutingEntry(node.key, node.key, 0, str(node.key) + '-002'))
+        for node in self.nodes:
+            for edge in self.edges:
+                if node.key == edge[0]:
+                    node.table[self.nodes[edge[1]].key].metric = 1
+                    node.table[self.nodes[edge[1]].key].next_hop = self.nodes[edge[1]].key
+                    self.nodes[edge[1]].table[node.key].metric = 1
+                    self.nodes[edge[1]].table[node.key].next_hop = node.key
+
+        for node in self.nodes:
+            self.broadcast_to_neighbours(node)
+
+    def add_one_node(self):
+        self.nodes.append(self.get_new_node(
+            1, self.window_width, 1, self.window_height))
+
+    def get_new_node(self, min_x, max_x, min_y, max_y):
+        x = random.randint(min_x, max_x)
+        y = random.randint(min_y, max_y)
+
+        new_node_key = 0
+        if len(self.nodes) > 0:
+            new_node_key = self.nodes[len(self.nodes) - 1].key + 1
+        return Node(new_node_key, x, y)
 
     def get_edges(self, nodes, max_edge_distance):
         edges = []
@@ -72,10 +222,51 @@ class GraphGenerator(FigureCanvas):
                     node2 = nodes[j]
                     distance = self.calculate_distance(
                         node1.x, node1.y, node2.x, node2.y)
-                    if distance < max_edge_distance and (j, i) not in edges:
-                        edges.append((i, j))
+                    if distance < max_edge_distance and (node2.key, node1.key) not in edges:
+                        edges.append((node1.key, node2.key))
                         edge_distances.append(distance)
         return edges, edge_distances
+
+    def get_nodes(self, node_count, min_x, max_x, min_y, max_y):
+        nodes = []
+        for i in range(node_count):
+            x = random.randint(min_x, max_x)
+            y = random.randint(min_y, max_y)
+            new_node = Node(i, x, y)
+            nodes.append(new_node)
+        return nodes
+
+    def create_nodes(self):
+        self.nodes = self.get_nodes(
+            self.node_count, 1, self.window_width, 1, self.window_height)
+        self.edges, self.edge_distances = self.get_edges(
+            self.nodes, self.max_edge_distance)
+        #self.fill_table()
+
+class GraphGenerator(FigureCanvas):
+    def __init__(self, parent=None, width=10, height=8, dpi=100):
+        self.graph = nx.Graph()
+        self.max_x_displacement = 10
+        self.max_y_displacement = 10
+        self.node_count = 20
+        self.window_width = 300
+        self.window_height = 300
+        self.max_edge_distance = 50
+
+        self.n = Graph(self.node_count, self.window_height, self.window_width, self.max_edge_distance)
+
+        self.current_step = 0
+        self.nodes_timesteps = []
+
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(
+            self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
 
     def get_nodes_step(self, nodes, max_x_displacement, max_y_displacement):
         for i in range(len(nodes)):
@@ -85,57 +276,77 @@ class GraphGenerator(FigureCanvas):
             y += random.randint(-max_y_displacement, max_y_displacement)
             nodes[i].x = x
             nodes[i].y = y
+        self.nodes_timesteps.append(deepcopy(nodes))
         return nodes
-
-    def set_node_count(self, count):
-        self.node_count = count
 
     def get_node_keys(self):
         result = []
-        for node in self.nodes:
+        for node in self.n.nodes:
             result.append(node.key)
         return result
-
-    def add_one_node(self):
-        self.nodes.append(self.add_node(1, self.window_width, 1, self.window_height))
-
-    def add_node(self, min_x, max_x, min_y, max_y):
-        x = random.randint(min_x, max_x)
-        y = random.randint(min_y, max_y)
-        return Node(len(self.nodes), x, y)
-
-    def create_nodes(self):
-        self.nodes = self.get_nodes(
-            self.node_count, 1, self.window_width, 1, self.window_height)
 
     def draw_next(self, step=True):
         self.get_next_step(step)
 
-    def get_next_step(self, step=True):
+    def check_edges(self, timestep):
+        if len(self.nodes_timesteps) > 1:
+            previous_nodes = self.nodes_timesteps[timestep - 2]
+            previous_edges, __ = self.n.get_edges(previous_nodes, self.n.max_edge_distance)
+            removed_edges =  set(previous_edges) - set(self.n.edges)
+            new_edges =  set(self.n.edges) - set(previous_edges)
+
+            for edge in removed_edges:
+                self.n.broadcast_broken_link(self.n.get_node_by_key(edge[0]), self.n.get_node_by_key(edge[1]))
+            for edge in new_edges:
+                self.n.broadcast_new_link(self.n.get_node_by_key(edge[0]), self.n.get_node_by_key(edge[1]))
+        
+    def jump_in_time(self, timestep=0):
+        self.get_next_step(step=False, timestep=timestep)
+
+    def get_next_step(self, step=True, timestep=0):
         self.graph.clear()
         self.axes.cla()
         plt.clf()
 
         # Add nodes and edges
         if step:
-            self.nodes = self.get_nodes_step(
-                self.nodes, self.max_x_displacement, self.max_y_displacement)
-        self.edges, self.edge_distances = self.get_edges(
-            self.nodes, self.max_edge_distance)
+            self.n.nodes = self.get_nodes_step(
+                self.n.nodes, self.max_x_displacement, self.max_y_displacement)
+        elif self.n.node_count > len(self.n.nodes):
+            self.n.add_one_node()
+            self.n.add_to_table(self.n.nodes[-1])
+            self.nodes_timesteps.append(deepcopy(self.n.nodes))
+        elif self.n.node_count < len(self.n.nodes):
+            self.nodes_timesteps.append(deepcopy(self.n.remove_random_node()))
+        try:
+            self.n.nodes = deepcopy(self.nodes_timesteps[timestep-1])
+        except:
+            pass
+        self.n.edges, self.n.edge_distances = self.n.get_edges(
+            self.n.nodes, self.n.max_edge_distance)
+        if(len(self.nodes_timesteps) == 1):
+            self.n.fill_table()
 
         node_keys = self.get_node_keys()
         self.graph.add_nodes_from(node_keys)
-        for i in range(len(self.nodes)):
-            self.graph.nodes[i]['pos'] = (self.nodes[i].x, self.nodes[i].y)
+        for i, node in enumerate(self.n.nodes):
+            self.graph.nodes[node.key]['pos'] = (
+                self.n.nodes[i].x, self.n.nodes[i].y)
 
-        self.graph.add_edges_from(self.edges)
+
+        self.graph.add_edges_from(self.n.edges)
+
+        self.n.periodic_update()
+        self.check_edges(timestep)
+        self.n.periodic_update()
 
         labels = {}
         for key in node_keys:
             labels[key] = key
 
-
         # Draw graph
         pos = nx.get_node_attributes(self.graph, 'pos')
         nx.draw(self.graph, pos, labels=labels, ax=self.axes)
         self.draw()
+        if timestep == 0:
+            self.current_step += 1
--- routing/main.py
+++ routing/main.py
@@ -1,22 +1,73 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QAction, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QSpacerItem, QSizePolicy, QPushButton
-from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import QSize
-
+from PyQt5.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QTableWidgetSelectionRange, QWidget, QMainWindow, QAction, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QSpacerItem, QSizePolicy, QPushButton, QSlider
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
+import math
 
 import sys
 import random
 import time
 import threading
 import asyncio
+
+from networkx.classes import graph
 from graph_generator import GraphGenerator
+
+class WinTable(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.title = "Table for node {}".format(0)
+        self.top    = 50
+        self.left   = 1450
+        self.width  = 450
+        self.height = 960
+        self.index = 0
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        
+        self.createTable()
+
+        # Add box layout, add table to box layout and add box layout to widget
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tableWidget) 
+        self.setLayout(self.layout)
+        try:
+            index = [i for i,__ in enumerate(mainWin.graph.n.nodes) if mainWin.graph.n.nodes[i].key == self.index][0]
+        except:
+            index = 0
+        self.setWindowTitle("Table for node {}".format(index))
+        self.show()
+
+    def createTable(self):
+       # Create table
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(len(mainWin.graph.n.nodes))
+        self.tableWidget.setColumnCount(4)
+        self.tableWidget.verticalHeader().setVisible(False)
+        self.tableWidget.setHorizontalHeaderLabels(['Destination', 'Next hop', 'Metric', 'Sequence number'])
+        self.tableWidget.setEditTriggers(QTableWidget.NoEditTriggers)
+        try:
+            index = [i for i,__ in enumerate(mainWin.graph.n.nodes) if mainWin.graph.n.nodes[i].key == self.index][0]
+        except:
+            index = 0
+        for x, entry in enumerate(mainWin.graph.n.nodes[index].table):
+            self.tableWidget.setItem(x,0, QTableWidgetItem(str(entry.destination)))
+            self.tableWidget.setItem(x,1, QTableWidgetItem(str(entry.next_hop)))
+            self.tableWidget.setItem(x,2, QTableWidgetItem(str(entry.metric)))
+            self.tableWidget.setItem(x,3, QTableWidgetItem(str(entry.sequence_number)))
+            self.tableWidget.move(0,0)
 
 class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
+        self.setMinimumSize(500, 350) 
 
         self.title = 'Ad-hoc networks routing protocol visualisation'
         self.left = 50
@@ -25,13 +76,14 @@ class MainWindow(QMainWindow):
         self.height = 800
         self.graph = GraphGenerator()
         self.simulating = False
+        self.timestep = 0
 
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         self.statusBar().showMessage('Ready for simulation')
 
-        widget =  QWidget(self)
+        widget = QWidget(self)
         self.setCentralWidget(widget)
         vlay = QVBoxLayout(widget)
         hlay = QHBoxLayout()
@@ -50,50 +102,116 @@ class MainWindow(QMainWindow):
         self.create_button.clicked.connect(self.create_nodes)
         self.add_node_button = QPushButton('Add one node', self)
         self.add_node_button.clicked.connect(self.add_node)
+        self.remove_random_button = QPushButton('Remove random node', self)
+        self.remove_random_button.clicked.connect(self.remove_random_node)
         self.start_simulation_button = QPushButton('Start simulation', self)
         self.start_simulation_button.clicked.connect(self.start_simulation)
         self.stop_simulation_button = QPushButton('Stop simulation', self)
         self.stop_simulation_button.clicked.connect(self.stop_simulation)
+        self.show_table_button = QPushButton('Show table for node:', self)
+        self.show_table_button.clicked.connect(self.show_table)
+
+        self.table_index = QLineEdit(self)
+        self.table_index.setFixedWidth(20)
+        self.table_index.setText("")
+
         hlay2 = QHBoxLayout()
         hlay2.addWidget(self.create_button)
         hlay2.addWidget(self.add_node_button)
+        hlay2.addWidget(self.remove_random_button)
+        hlay2.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
         hlay2.addWidget(self.start_simulation_button)
         hlay2.addWidget(self.stop_simulation_button)
-        hlay2.addItem(QSpacerItem(1000, 10, QSizePolicy.Expanding))
+        hlay2.addWidget(self.show_table_button)
+        hlay2.addWidget(self.table_index)
         vlay.addLayout(hlay2)
         self.graph = GraphGenerator(self, width=5, height=4, dpi=100)
         vlay.addWidget(self.graph)
+        hlay3 = QHBoxLayout()
+        self.backwards_button = QPushButton('Go back in time', self)
+        self.backwards_button.clicked.connect(self.backwards)
+        self.forwards_button = QPushButton('Go forward in time', self)
+        self.forwards_button.clicked.connect(self.forwards)
+
+
+        hlay3.addWidget(self.backwards_button)
+        hlay3.addItem(QSpacerItem(100, 10, QSizePolicy.Expanding))
+        hlay3.addWidget(self.forwards_button)
+        vlay.addLayout(hlay3)
+
+        hlay4 = QHBoxLayout()
+        self.label = QLabel()
+        self.label.setText("Current step: " + str(1))
+        hlay4.addItem(QSpacerItem(50, 10, QSizePolicy.Expanding))
+        hlay4.addWidget(self.label)
+        hlay4.addItem(QSpacerItem(50, 10, QSizePolicy.Expanding))
+        vlay.addLayout(hlay4)
 
         self.stop_simulation_button.setEnabled(False)
+        self.backwards_button.setEnabled(False)
+        self.forwards_button.setEnabled(False)
 
-        self.graph.create_nodes()
+        self.graph.n.create_nodes()
         self.graph.draw_next()
 
     def create_nodes(self):
         number_of_nodes = 10
         if self.line.text().isnumeric():
             number_of_nodes = int(self.line.text())
-        self.graph.set_node_count(number_of_nodes)
-        self.graph.create_nodes()
+        self.graph.n.set_node_count(number_of_nodes)
+        self.graph.n.create_nodes()
+        self.graph.nodes_timesteps.clear()
         self.graph.draw_next()
 
     def add_node(self):
-        self.graph.node_count += 1
-        self.graph.add_one_node()
+        self.graph.n.node_count += 1
+        self.graph.draw_next(step=False)
+
+    def backwards(self):
+        if self.graph.current_step - 1 > abs(self.timestep):
+            self.timestep -= 1
+            self.graph.jump_in_time(timestep = self.timestep)
+        else:
+            self.backwards_button.setEnabled(False)
+        if self.timestep < 0:
+            self.forwards_button.setEnabled(True)
+        self.label.setText("Current step: " + str(self.graph.current_step + self.timestep))
+    
+    def forwards(self):
+        if self.timestep < 0:
+            self.forwards_button.setEnabled(True)
+            self.timestep += 1
+            self.graph.jump_in_time(timestep = self.timestep)
+        else:
+            self.forwards_button.setEnabled(False)
+        if self.graph.current_step - 1> abs(self.timestep):
+            self.backwards_button.setEnabled(True)
+        self.label.setText("Current step: " + str(self.graph.current_step + self.timestep))
+
+
+    def remove_random_node(self):
+        self.graph.n.node_count -= 1
         self.graph.draw_next(step=False)
 
     def start_simulation(self):
         def step():
             self.graph.draw_next()
+            self.label.setText("Current step: " + str(self.graph.current_step + self.timestep))
             time.sleep(1)
-            if self.simulating:
-                step()
+            if self.simulating: 
+                if self.timestep < 0:
+                    self.forwards()
+                    step()
+                else:
+                    step()
 
         self.statusBar().showMessage('Simulating...')
         self.simulating = True
         self.create_button.setEnabled(False)
         self.start_simulation_button.setEnabled(False)
         self.stop_simulation_button.setEnabled(True)
+        self.backwards_button.setEnabled(True)
+        self.forwards_button.setEnabled(True)
 
         simulation_timer = threading.Thread(target=step)
         simulation_timer.start()
@@ -104,6 +222,16 @@ class MainWindow(QMainWindow):
         self.create_button.setEnabled(True)
         self.start_simulation_button.setEnabled(True)
         self.stop_simulation_button.setEnabled(False)
+
+    def show_table(self):
+        index = 0
+        if self.table_index.text().isnumeric():
+            index = int(self.table_index.text())
+        self.winTable = WinTable()
+        self.winTable.index = index
+        self.winTable.initUI()
+
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
